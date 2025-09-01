aaa8fbd1b46f4cf08830e30226e7b7a881677b1f
marandege <marandege@gmail.com>
2025-09-01 13:20:26 +0200

    new tab name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
+
 import "./App.css";
 import Game from "./screens/Game";
 
 function App() {
+  useEffect(() => {
+    document.title = "Tick Tack Toe v0.1";
+  }, []); // The empty array makes it run only once when the component mounts
+
   return <Game />;
 }
 
